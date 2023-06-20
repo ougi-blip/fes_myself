3ec3aefb272cb2728bcf694bf5085d1bdffac2fb
ougi <ac01396@ms.aichi-c.ed.jp>
2023-06-20 15:55:18 +0900

    modified:   www/index.html 	modified:   www/reserve.js

--- www/reserve.js
+++ www/reserve.js
@@ -1,2 +1,23 @@
 // This is a JavaScript file
 
+let ReserveStore = ncmb.DataStore("RSVclass");
+RserveStore.set("message", "Hello, NCMB!")
+  .save()
+  .then(function(object){
+      // 保存に成功した場合の処理
+
+  })
+  .catch(function(err){
+      // 保存に失敗した場合の処理
+
+  });
+  
+
+
+
+
+function showModal() {
+  var modal = document.querySelector('ons-modal');
+  modal.show();
+  setTimeout(function() { modal.hide(); }, 2000);
+}
